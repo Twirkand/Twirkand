757ec9a4a8b61ef50b20286427fd5dae4c23f48b
Twirkand <146337425+Twirkand@users.noreply.github.com>
2025-11-27 17:02:54 +0000

    Create Bravo.sh

---
+++ Bravo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Desmutea y ajusta el volumen del sink de audio
+#pactl set-sink-mute @DEFAULT_SINK@ 0
+#pactl set-sink-volume @DEFAULT_SINK@ 150%
+
+# Descarga el archivo MP3 y la imagen
+wget -P ~/Vídeos https://github.com/Twirkand/Twirkand/raw/main/nina.mp3
+wget -P ~/Vídeos https://github.com/Twirkand/Twirkand/raw/main/nina.jpeg
+
+# Reproduce el audio
+rhythmbox ~/Vídeos/nina.mp3 &
+
+# Muestra la imagen a pantalla completa usando feh
+feh --fullscreen ~/Vídeos/nina.jpeg

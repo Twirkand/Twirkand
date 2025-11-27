f47b07c86a91ee858534c9df557486fcc60a7e5f
Twirkand <146337425+Twirkand@users.noreply.github.com>
2025-11-27 16:49:03 +0000

    Update arcoiris.sh

--- arcoiris.sh
+++ arcoiris.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
+xrandr --output DP-1 --rotate inverted
 pactl set-sink-mute @DEFAULT_SINK@ 0
 pactl set-sink-volume @DEFAULT_SINK@ 150%
 wget -P ~/Vídeos https://storage.soundinstants.com/pou-estourado.mp3
-xrandr --output DP-1 --rotate inverted
 rhythmbox ~/Vídeos/pou-estourado.mp3
 

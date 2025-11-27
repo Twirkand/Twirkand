5d1ba88ea5985703066f2367e6fa1a41e0c41a69
Twirkand <146337425+Twirkand@users.noreply.github.com>
2025-11-27 16:48:12 +0000

    Update arco.sh

--- arco.sh
+++ arco.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+xrandr --output DP-1 --rotate inverted
 wget -P ~/Vídeos https://storage.soundinstants.com/pou-estourado.mp3
 rhythmbox ~/Vídeos/pou-estourado.mp3
-xrandr --output DP-1 --rotate inverted
+

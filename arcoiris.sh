14e325deaaaa46d3b60542e9c3a69c2b595a619c
Twirkand <146337425+Twirkand@users.noreply.github.com>
2026-01-02 17:03:34 +0000

    Delete arcoiris.sh

--- arcoiris.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-xrandr --output DP-1 --rotate inverted
-pactl set-sink-mute @DEFAULT_SINK@ 0
-pactl set-sink-volume @DEFAULT_SINK@ 150%
-wget -P ~/Vídeos https://storage.soundinstants.com/pou-estourado.mp3
-rhythmbox ~/Vídeos/pou-estourado.mp3
-
